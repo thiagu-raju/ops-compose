432447c838d197b3cc211ef1ef1c402085be487e
Efra Peralta <efra23peralta@gmail.com>
2023-01-09 22:58:27 -0400

    feat: extract certs from gateway

---
+++ certs/extract-certs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+ACME="/certs/data/acme.json"
+DOMAINS_DIR="/certs/domains"
+
+if [ ! -f "$ACME" ]; then
+  echo "🚨 Oops! acme.json file not found"
+  exit 1
+fi
+
+echo "🚀 Extracting certificates from acme.json..."
+
+DOMAINS=$(jq -r ".certs.Certificates[].domain.main" $ACME | sort | uniq)
+
+for DOMAIN in $DOMAINS; do
+  echo "📦 Extracting $DOMAIN certificate..."
+
+  jq -r ".certs.Certificates[] | select(.domain.main==\""$DOMAIN"\") | .certificate" $ACME | base64 --decode >$DOMAINS_DIR/$DOMAIN.crt
+  jq -r ".certs.Certificates[] | select(.domain.main==\""$DOMAIN"\") | .key" $ACME | base64 --decode >$DOMAINS_DIR/$DOMAIN.key
+
+  echo "✅ $DOMAIN certificate extracted"
+done
+
+echo "🎉 All certificates extracted"
