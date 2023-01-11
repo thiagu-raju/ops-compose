ff2cac6704bb650d384fa29ece09ae9075652820
Efra Peralta <efra23peralta@gmail.com>
2023-01-11 11:31:40 -0400

    fix: docker-compose creates directory instead of file

--- certs/extract-certs.sh
+++ certs/extract-certs.sh
@@ -2,39 +2,30 @@
 
 set -e
 
-ACME="/data/acme.json"
+ACME="/certs/data/acme.json"
+DOMAINS_DIR="/certs/domains"
 
 if [ ! -f "$ACME" ]; then
   echo "🚨 Oops! acme.json file not found"
   exit 1
 fi
 
-if [ ! -d "vault" ]; then
-  mkdir vault
-fi
-
 echo "🚀 Extracting certificates from acme.json..."
-
-EXTRACT=$1
 
 DOMAINS=$(jq -r ".certs.Certificates[].domain.main" $ACME | sort | uniq)
 
 for DOMAIN in $DOMAINS; do
-  if [ "$EXTRACT" != "" ] && [ "$EXTRACT" != "$DOMAIN" ]; then
-    continue
-  fi
-
   echo "📦 Getting $DOMAIN certificate..."
 
   echo "📄 Extracting certificate..."
   {
     jq -r ".certs.Certificates[] | select(.domain.main==\"""$DOMAIN""\") | .certificate" $ACME | base64 --decode
-  } >vault/tls.crt
+  } >"$DOMAINS_DIR/$DOMAIN.crt"
 
   echo "🔑 Extracting private key..."
   {
     jq -r ".certs.Certificates[] | select(.domain.main==\"""$DOMAIN""\") | .key" $ACME | base64 --decode
-  } >vault/tls.key
+  } >"$DOMAINS_DIR/$DOMAIN.key"
 
   echo "✅ $DOMAIN certificate extracted"
 done
